3e2f00a8753767e32fbadceeaaae6fc3712cc3c4
hrdk224 <hardikkille07@gmail.com>
2026-02-21 13:28:41 +0000

    added delete button and render button functionality

--- index.js
+++ index.js
@@ -2,14 +2,29 @@ const saveBtn = document.getElementById("save-btn")
 const tabBtn = document.getElementById("tab-btn")
 const deleteBtn = document.getElementById("delete-btn")
 const inputEl = document.getElementById("input")
+const ulEl = document.getElementById("ul-el")
 let leads = []
 
 
 saveBtn.addEventListener("click", function () {
     leads.push(inputEl.value)
-    render()
+    inputEl.value = " "
+    render(leads)
 })
 
-function render() {
-    console.log(leads)
+deleteBtn.addEventListener("dblclick", function () {
+    leads = []
+    render(leads)
+})
+
+function render(arr) {
+    let lead_tags = ""
+    for (let i = 0; i < arr.length; i++) {
+        lead_tags += `<li>
+        <a href ="${arr[i]}" target='_blank'>
+        ${arr[i]}
+        </a>
+        </li>`
+    }
+    ulEl.innerHTML = lead_tags
 }
